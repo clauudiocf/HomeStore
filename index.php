1c216bc77ad2c50ada2cebcebc97a5cdffcf6859
cetecom <cetecom@LC08L1208>
2017-06-22 13:08:31 -0400

    segundo commit trabajo 3

--- index.php
+++ index.php
@@ -41,6 +41,36 @@ and open the template in the editor.
         </div>
         <br>
         <div id="piedepagina"></div>
+        <form id="frmusuario" action="controlador/ValidaUsuario.php" method="post">
+            <div><label>Usuario:</label><input id="nomusuario" type="text" name="nomusuario" ></div>
+            <div><label>Clave:</label><input id="clave" type="password" name="clave" ></div>
+            <input id="enviar" type="button" onclick="" value="Enviar"> 
+            <div id="mensaje"></div>
+        </form>
+
+    </body>
+    <script>
+    $(document).ready(function(){
+            $("#enviar").click(function(){
+                /*$("form").hide();
+                alert("Ocultaste el formulario ;-) "+ $("#nomusuario").val());*/
         
+                if ($("#nomusuario").val()!="" && $("#clave").val()!=""){
+                    ///*$("#frmusuario").submit();
+                        $.ajax({url:"controlador/ValidaUsuario.php"
+                            ,type:'post'
+                            ,data:{'nomusuario':$("#nomusuario").val(),
+                                'clave':$("#clave").val()
+                                }
+                            ,success:function(resultado){
+                                $("#mensaje").html(resultado);
+                            }
+                        });
+                    }//Cierre IF Valida blancos
+                else
+                    alert("Debe Agregar el usuario y clave");
+            });//Click Boton enviar
+     });//Function Ready de la página
+     </script>
     </body>
 </html>
